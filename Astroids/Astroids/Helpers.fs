eac7399934db48bc07643fcc2e43fb727ec44363
bart-rijnders <mail@bartrijnders.com>
2016-11-21 22:32:32 +0100

    added xnb files for sprites

--- Astroids/Astroids/Helpers.fs
+++ Astroids/Astroids/Helpers.fs
@@ -3,9 +3,3 @@
 open Microsoft.Xna.Framework.Graphics
 open Microsoft.Xna.Framework
 
-let spriteLoader (path) graphics =
-    use imagePath = System.IO.File.OpenRead(path)
-    let texture = Texture2D.FromStream(graphics, imagePath)
-    let textureData = Array.create<Color> (texture.Width * texture.Height) Color.Transparent
-    texture.GetData(textureData)
-    texture
