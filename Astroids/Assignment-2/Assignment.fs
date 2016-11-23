04e8504ff2c8cc7e7dbbaabe2d625cf77f99f026
ThomOverhand <Thom_overhand@hotmail.com>
2016-11-23 17:38:37 +0100

    Assignment 3 not implemented

---
+++ Astroids/Assignment-2/Assignment.fs
@@ -0,0 +1,40 @@
+﻿module Assignment
+
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Input
+open Microsoft.Xna.Framework
+
+// Assignment A
+//
+// check if the screen contains the spaceship
+// the size of the ship is 50.0f * 50.0f
+// the size of the screen is 1920.0f * 1080.0f
+// if the ship is outside the screen, its position should be set to the opposite side of the screen
+// change 0.0f to the correct values
+
+let containInsideScreen(position: Vector2) =
+    let x =
+        if position.X > 0.0f + 50.0f then
+            -50.0f
+        elif position.X < -50.0f then
+            0.0f + 50.0f
+        else 
+            position.X
+    let y =
+        if position.Y > 0.0f + 50.0f then
+            -50.0f
+        elif position.Y < -50.0f then
+            0.0f + 50.0f
+        else 
+            position.Y
+    new Vector2(x, y)
+
+// Assignment B
+//
+// the deltaTime is the time between now and the last procedure executed
+// the cooldown is the time the missiles can't shoot
+// we want to know the new cooldown  
+
+
+let updateMissileCooldown(cooldown : float, deltaTime : float) =
+    -0.1f
---
+++ Astroids/Assignment-3/Helpers.fs
@@ -0,0 +1,54 @@
+﻿module Helpers
+
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Input
+open Microsoft.Xna.Framework
+
+let MakeRect (texture : Texture2D, position : Vector2) = 
+        let width = texture.Width
+        let height = texture.Height
+        let left = (int)position.X - (width / 2)
+        let top = (int)position.Y - (height / 2)
+        let rect = new Rectangle(left, top, width, height)
+        rect
+
+let isInsideScreen(position: Vector2) = 
+    position.X > 0.0f && position.X < 1920.0f && position.Y > 0.0f && position.Y < 1080.0f
+
+
+let screenLoop(position: Vector2) =
+    let x' : float32 =
+        if position.X > 1920.0f + 50.0f then
+            -50.0f
+        elif position.X < -50.0f then
+            1920.0f + 50.0f
+        else 
+            position.X
+
+    let y' : float32 =
+        if position.Y >  1920.0f + 50.0f then
+            -50.0f
+        elif position.Y < -50.0f then
+            1080.0f + 50.0f
+        else 
+            position.Y
+    new Vector2(x', y')
+
+
+let CheckCollision(rect : Rectangle, rectlist : Rectangle list) = 
+    not (List.exists(fun (recti : Rectangle) -> rect.Intersects(recti)) rectlist)
+     
+let checkInput key onPressed onNotPressed = 
+    let ks = Keyboard.GetState()
+    match ks.[key] with
+            | KeyState.Down -> onPressed
+            | _ -> onNotPressed
+
+let direction rotation =
+    new Vector2(sin(rotation), (cos(rotation) * -1.0f))
+
+let updateTimer defaultTime currentTime (dt: GameTime) =
+    if currentTime < 0.0 then
+        defaultTime
+    else
+        currentTime - dt.ElapsedGameTime.TotalMilliseconds
